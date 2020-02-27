bc24ae2518b4f4043f02032a2a03ff6382aad745
sdreher7991 <61548953+sdreher7991@users.noreply.github.com>
2020-02-27 14:36:01 +0100

    Create siem.py

---
+++ siem.py
@@ -0,0 +1,11 @@
+import super_siem as siem
+
+# Initialize SIEM
+siem.attack_detected = False
+
+# Monitor network
+for siem.attack_detected == False:
+  siem.check_for_attack
+ 
+# Attack detected
+siem.alarm
